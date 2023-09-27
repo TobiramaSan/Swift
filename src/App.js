f77ac5c0d0fb014b804b8b243eb9e7b33a27d4be
TobiramaSan <alademehintobi1@gmail.com>
2023-09-27 16:38:44 +0100

    added changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./pages/home/home";
 import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing/landing";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/landing" element={<Landing />} />
       </Routes>
     </div>
   );
---
+++ src/pages/landing/landing.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "../../assets/whitelogoo.png";
+import square1 from "../../assets/square1.png";
+import square2 from "../../assets/square2.png";
+import square3 from "../../assets/square3.png";
+import square4 from "../../assets/square4.png";
+import wave from "../../assets/wave.png";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <div className="nav">
+        <div className="swift">
+          <div className="logo-name">
+            <div className="main-image">
+              <div className="img">
+                <img src={logo} alt="" />
+              </div>
+              <div className="name">
+                <h1>Swift</h1>
+                <span>e-Learning</span>
+              </div>
+            </div>
+          </div>
+          <p>Most affordable and friendly E-Learning Platform</p>
+        </div>
+
+        <div className="squares">
+          <img src={square1} alt="" />
+          <img src={square2} alt="" />
+          <img src={square3} alt="" />
+          <img src={square4} alt="" />
+        </div>
+      </div>
+      <div className="landing-content">
+        <div className="welcome">
+          <div className="wave">
+            <h1>Welcome to Swift e-Learning</h1>
+            <img src={wave} alt="" />
+          </div>
+          <p>Creating learning experiences to your doorstep!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
